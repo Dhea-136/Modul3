8527642006bb2eb8f83049f230770ae4749da6e7
Dhea-136 <dhearengganis52@gmail.com>
2025-10-24 12:10:50 +0700

    commit

---
+++ src/Codelab1.java
@@ -0,0 +1,7 @@
+public class Codelab1 {
+    public static void main(String[] args) {
+        String kalimat1 = "saya bapak Budi";
+        String kalimat2 = "saya ibu Budi";
+        System.out.println("Berikut: " + kalimat1 + " " + kalimat2);
+    }
+}
