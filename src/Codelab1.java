e0fa0d73ec88d5c337f3641d9e04434ddf616f55
Dhea-136 <dhearengganis52@gmail.com>
2025-10-24 14:15:52 +0700

    ini update lalala

--- src/Codelab1.java
+++ src/Codelab1.java
@@ -4,5 +4,6 @@ public class Codelab1 {
         String kalimat2 = "saya ibu Budi";
         String kalimat3 = "saya adik Budi";
         System.out.println("Berikut: " + kalimat1 + " " + kalimat2 + " " + kalimat3);
+        System.out.println("APA AJA");
     }
 }
